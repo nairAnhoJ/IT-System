56cb39bf997b67f5af0071678b51d772e0b3c886
John Arian <malondrasarian@gmail.com>
2023-03-30 10:01:31 +0800

    Update

--- routes/web.php
+++ routes/web.php
@@ -72,10 +72,6 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
     Route::get('/change-password', [UserController::class, 'changePass'])->name('changePass');
 
     // Ticketing
