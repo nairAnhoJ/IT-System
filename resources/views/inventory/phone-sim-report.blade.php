1a43d5aafab6b13b2ef2f029d3bd19ab5a36ea26
John Arian <malondrasarian@gmail.com>
2023-07-28 16:50:03 +0800

    Update

--- resources/views/inventory/phone-sim-report.blade.php
+++ resources/views/inventory/phone-sim-report.blade.php
@@ -4,6 +4,7 @@
     @endsection
 
     <div style="height: calc(100vh - 65px);" class="p-3 text-gray-200 w-screen">
+        <h1 class="mb-5 text-3xl font-bold">PHONE / SIM REPORT</h1>
         <form action="{{ route('phoneSim.report.generate') }}" method="POST">
             @csrf
             <div class="mb-5">
@@ -37,7 +38,7 @@
                 </div>
             </div>
 
-            <button type="submit" class="text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">Submit</button>
+            <button type="submit" class="text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">GENERATE</button>
         </form>
     </div>
 
