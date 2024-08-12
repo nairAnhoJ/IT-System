25cd8311cc6f7d4b5e844cbbc6a905918201568e
unknown <malondrasarian@gmail.com>
2024-08-12 16:51:32 +0800

    Update

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -40,7 +40,7 @@ class TicketController extends Controller
                 ->where('department', $userDeptID)
                 ->orderBy('status', 'desc')
                 ->orderBy('id', 'desc')
-                ->limit(131)
+                ->limit(136)
                 ->get();
         }else{
             $tickets = Ticket::with('requestor', 'departmentRow', 'category', 'assigned')
