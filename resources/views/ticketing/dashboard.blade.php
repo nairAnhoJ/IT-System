eeaebaa71b20ee23e04691e9d419c1f4cbfae0a3
unknown <malondrasarian@gmail.com>
2026-03-26 17:32:32 +0800

    update text size

--- resources/views/ticketing/dashboard.blade.php
+++ resources/views/ticketing/dashboard.blade.php
@@ -118,7 +118,7 @@
 
                             {{-- Ticket Info --}}
                                 <p id="ticketSubject" class="mb-2 text-sm font-semibold leading-relaxed text-gray-300"></p>
-                                <div id="ticketDesc" class="mb-2 max-h-[200px] text-sm leading-relaxed text-gray-300 whitespace-pre-line overflow-y-auto"></div>
+                                <div id="ticketDesc" class="mb-2 max-h-[180px] text-sm leading-relaxed text-gray-300 whitespace-pre-line overflow-y-auto"></div>
                             {{-- Ticket Info --}}
 
                             {{-- Attachment Buttons --}}
@@ -140,7 +140,7 @@
                                 <div id="ticketUpdateDiv">
                                     <hr class="my-5">
                                     <label for="ticketUpdateTextArea" class="block text-base font-medium text-white">Update</label>
-                                    <textarea disabled id="ticketUpdateTextArea" style="resize: none;" rows=4 cols=50 maxlength=1000 class="UpdateDivAutoHeight block max-h-[160px] p-2.5 w-full text-sm rounded-lg bg-gray-700 border-gray-700 placeholder-gray-400 text-white">${update}</textarea>
+                                    <textarea disabled id="ticketUpdateTextArea" style="resize: none;" rows=3 cols=50 maxlength=1000 class="UpdateDivAutoHeight block max-h-[160px] p-2.5 w-full text-sm rounded-lg bg-gray-700 border-gray-700 placeholder-gray-400 text-white">${update}</textarea>
                                 </div>
                                 <div id="ticketUpdateInputDiv" class="flex gap-x-3">
                                     <input type="text" id="ticketUpdateInput" name="ticketUpdate" class="w-full first-letter:block p-2.5 text-sm rounded-lg bg-gray-700 border border-gray-300 placeholder-gray-400 text-white" placeholder="Update here..." autocomplete="off">
@@ -156,7 +156,7 @@
                                         <label class="block mt-5 mb-2 text-base font-medium text-white">
                                             Resolution <span class="text-red-500 text-sm">*Required upon completion</span>
                                         </label>
-                                        <textarea style="resize: none;" id="ticketResolutionInput" name="ticketResolution" rows=2 cols=50 maxlength=1000 class="block p-2.5 w-full text-sm rounded-lg border bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"></textarea>
+                                        <textarea style="resize: none;" id="ticketResolutionInput" name="ticketResolution" rows=1 cols=50 maxlength=1000 class="block p-2.5 w-full text-sm rounded-lg border bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"></textarea>
                                         <div class="mt-5">
                                             <label for="attachment" class="block text-sm font-medium text-white">Upload Attachment</label>
                                             <div class="col-span-5">
