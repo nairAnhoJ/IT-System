316146e704e18901f78395f982a547b28d325544
unknown <malondrasarian@gmail.com>
2026-05-12 10:25:00 +0800

    update href

--- resources/views/ticketing/dashboard.blade.php
+++ resources/views/ticketing/dashboard.blade.php
@@ -499,8 +499,10 @@
                 <div class="h-8 col-span-2">
                     {{-- @if ($userDept != 'IT') --}}
                     @if (auth()->user()->dept_id != $deptInCharge)
-                        <a href="{{ route('ticket.create') }}" target type="button" class="w-40 mb-2 mr-2 text-sm font-medium leading-8 text-center text-white bg-blue-600 rounded-lg focus:ring-4 hover:bg-blue-700 focus:outline-none focus:ring-blue-800">Create Ticket</a>
-                        <a href="{{ route('sap.index') }}" type="button" class="w-40 mb-2 mr-2 text-sm font-medium leading-8 text-center text-white bg-blue-600 rounded-lg focus:ring-4 hover:bg-blue-700 focus:outline-none focus:ring-blue-800">SAP BP</a>
+                        $host = $_SERVER['SERVER_NAME'];
+                        <a href="http://{{ $_SERVER['SERVER_NAME'] }}:1111" type="button" class="w-40 mb-2 mr-2 text-sm font-medium leading-8 text-center text-white bg-blue-600 rounded-lg focus:ring-4 hover:bg-blue-700 focus:outline-none focus:ring-blue-800">Create Ticket</a>
+                        <!-- <a href="{{ route('ticket.create') }}" target type="button" class="w-40 mb-2 mr-2 text-sm font-medium leading-8 text-center text-white bg-blue-600 rounded-lg focus:ring-4 hover:bg-blue-700 focus:outline-none focus:ring-blue-800">Create Ticket</a>
+                        <a href="{{ route('sap.index') }}" type="button" class="w-40 mb-2 mr-2 text-sm font-medium leading-8 text-center text-white bg-blue-600 rounded-lg focus:ring-4 hover:bg-blue-700 focus:outline-none focus:ring-blue-800">SAP BP</a> -->
                     @else
                         <a href="{{ route('ticket.createForIT') }}" type="button" class="w-40 mb-2 mr-2 text-sm font-medium leading-8 text-center text-white bg-blue-600 rounded-lg focus:ring-4 hover:bg-blue-700 focus:outline-none focus:ring-blue-800">Create Ticket</a>
                     @endif
